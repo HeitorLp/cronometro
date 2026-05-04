7353b345933ecf7f47569dcb70e41741dfdcbd0e
HeitorLp <heitor.pincelli@escola.pr.gov.br>
2026-05-04 15:08:18 +0000

    aula 2 possivel continuação

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].classList.add("ativo");
   };
 }
+for (var i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+var frutas = ["Maçã", "Banana", "Laranja", "Pera"];
+for (var i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
+let contador = 5;
+contador += 3;
+let contador = 10;
+contador -= 4;
+var numero = 3;
+numero *= 2;
+var numero = 10;
+numero /= 2; 
